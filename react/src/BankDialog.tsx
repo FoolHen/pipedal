5eba089f0f6d6eef1ccfbc73865615a6ef571704
FoolHen <fulgen.ruiz@outlook.com>
2024-07-07 21:19:51 +0200

    Fix banks white bg in dark mode

--- react/src/BankDialog.tsx
+++ react/src/BankDialog.tsx
@@ -293,7 +293,7 @@ const BankDialog = withStyles(styles, { withTheme: true })(
             let classes = this.props.classes;
             let selectedItem = this.isEditMode() ? this.state.selectedItem : this.state.banks.selectedBank;
             return (
-                <div key={bankEntry.instanceId} style={{ background: "white" }} >
+                <div key={bankEntry.instanceId}>
 
                     <ButtonBase style={{ width: "100%", height: 48 }}
                         onClick={() => this.handleItemClick(bankEntry.instanceId)}
